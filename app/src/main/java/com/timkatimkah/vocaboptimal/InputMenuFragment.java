fdf0f34fb356eec9b73c0bfbad3a15251e698394
tipie101 <timka.timkah@gmail.com>
2020-06-03 12:27:22 +0200

    New Feature: Read In TextFile / TODO: load vocabulary via txt-file

--- app/src/main/java/com/timkatimkah/vocaboptimal/InputMenuFragment.java
+++ app/src/main/java/com/timkatimkah/vocaboptimal/InputMenuFragment.java
@@ -1,6 +1,12 @@
 package com.timkatimkah.vocaboptimal;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,10 +14,16 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.google.android.material.snackbar.Snackbar;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class InputMenuFragment extends Fragment {
 
@@ -56,8 +68,29 @@ public class InputMenuFragment extends Fragment {
             }
         });
 
+        // Request Permission to read external file
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+            && getActivity().checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                getActivity().requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},  MainActivity.PERMISSION_REQUEST_STORAGE);
+        }
 
+        view.findViewById(R.id.save_vocab_from_txt).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                performFileSearch();
+            }
+        });
     }
+
+    private void performFileSearch() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        // TODO: declare own file extension
+        intent.setType("text/*");
+        getActivity().startActivityForResult(intent, MainActivity.READ_REQUEST_CODE);
+    }
+
+
 
 
 }
--- app/src/main/java/com/timkatimkah/vocaboptimal/MainActivity.java
+++ app/src/main/java/com/timkatimkah/vocaboptimal/MainActivity.java
@@ -1,21 +1,83 @@
 package com.timkatimkah.vocaboptimal;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
+    final static int PERMISSION_REQUEST_STORAGE = 1000;
+    final static int READ_REQUEST_CODE = 42;
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_STORAGE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission granted!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission denied!", Toast.LENGTH_SHORT).show();
+                // finish();
+            }
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        System.out.println("onActivityResult-----------------------------------------------------");
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == MainActivity.READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            if (data != null) {
+                Uri uri = data.getData();
+                String fileContent = readTextFile(uri);
+                Toast.makeText(this, fileContent, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private String readTextFile(Uri uri){
+        BufferedReader reader = null;
+        StringBuilder builder = new StringBuilder();
+        try {
+            reader = new BufferedReader(new InputStreamReader(getContentResolver().openInputStream(uri)));
+            String line = "";
+
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null){
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return builder.toString();
     }
 
 }
